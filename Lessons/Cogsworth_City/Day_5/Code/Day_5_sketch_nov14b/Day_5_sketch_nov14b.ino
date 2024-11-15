ced53e47ab98e18346280e66d41db57d929384b3
Steve Nielson <N5QC@arrl.net>
2024-11-15 14:02:20 -0600

    Days 4 and 5 ready for hardware

--- Lessons/Cogsworth_City/Day_5/Code/Day_5_sketch_nov14b/Day_5_sketch_nov14b.ino
+++ Lessons/Cogsworth_City/Day_5/Code/Day_5_sketch_nov14b/Day_5_sketch_nov14b.ino
@@ -1,65 +1,54 @@
 const int switchPin1 = 2;    // switc1 1 is connected to pin 2
-const int switchPin1 = 3;    // Switch 3 is connected to pin 3
+const int switchPin3 = 3;    // Switch 3 is connected to pin 3
+const int ledPin1 = 8;       // Led 1 is connected to pin 8
+const int ledPin2 = 9;       // Led 2 is connected to pin 9
 
+int switchState1 = 0;        // Variable to hold the state of switch 1
+int lastSwitchState1 = 0;    // Previous state of switch 1
 
+int switchState3 = 0;        // Variable to hold the state of switch 3
+int lastSwitchState3 = 0;    // Previous state of switch 3
 
-
-
-void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-
-
-
-
-
-const int ledPin1 = 8;      // LED 1 is connected to pin 8
-const int ledPin2 = 9;      // LED 2 is connected to pin 9
-
-int buttonState1 = 0;       // Variable to hold the state of button 1
-int lastButtonState1 = 0;   // Previous state of button 1
-int buttonState2 = 0;       // Variable to hold the state of button 2
-int lastButtonState2 = 0;   // Previous state of button 2
-int ledState1 = LOW;        // Current state of LED 1 (initially off)
-int ledState2 = LOW;        // Current state of LED 2 (initially off)
+int ledState1 = LOW;         // Current state of LED 1 (initially off)
+int ledState2 = LOW;         // Current state of LED 2 (initially off)
 
 void setup() {
-  pinMode(buttonPin1, INPUT);
-  pinMode(buttonPin2, INPUT);
+  pinMode(switchPin1, INPUT);
+  pinMode(switchPin3, INPUT);
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
+ 
 }
 
 void loop() {
-  // Read the state of button 1
-  buttonState1 = digitalRead(buttonPin1);
+  // Read the state of switch 1
+  switchState1 = digitalRead(switchPin1);
 
-  // Toggle LED 1 on/off when button 1 is pressed
-  if (buttonState1 != lastButtonState1) {
-    if (buttonState1 == HIGH) {
-      ledState1 = !ledState1;  // Toggle LED state
-      digitalWrite(ledPin1, ledState1);  // Set LED state
+
+  // Toggle LED 1 on/off when switch goes to on
+  if (switchState1 != lastSwitchState1)  {
+    if (switchState1 == HIGH)  {
+      ledState1 == !ledState1;  // Toggle LED state
+      digitalWrite(ledPin1, ledState1);  //Set LED state
     }
     delay(50);  // Debounce delay
   }
-  lastButtonState1 = buttonState1;  // Save the current state for next iteration
+  lastSwitchState1 = switchState1;  // Save the current state for the next iteration
 
-  // Read the state of button 2
-  buttonState2 = digitalRead(buttonPin2);
 
-  // Toggle LED 2 on/off when button 2 is pressed
-  if (buttonState2 != lastButtonState2) {
-    if (buttonState2 == HIGH) {
-      ledState2 = !ledState2;  // Toggle LED state
-      digitalWrite(ledPin2, ledState2);  // Set LED state
+    // Read the state of switch 3
+  switchState3 = digitalRead(switchPin3);
+
+
+  // Toggle LED 2 on/off when switch goes to on
+  if (switchState3 != lastSwitchState3)  {
+    if (switchState3 == HIGH)  {
+      ledState2 == !ledState2;  // Toggle LED state
+      digitalWrite(ledPin2, ledState2);  //Set LED state
     }
     delay(50);  // Debounce delay
   }
-  lastButtonState2 = buttonState2;  // Save the current state for next iteration
+  lastSwitchState3 = switchState3;  // Save the current state for the next iteration
+
 }
+
