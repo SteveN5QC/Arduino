b25e23f06ecaf9fc53ae57559048330d3a35f957
Steve Nielson <N5QC@arrl.net>
2024-11-20 18:27:14 -0600

    Updated through Day 7 Ready for Hardware

---
+++ Lessons/Template_Code/Template_Code.ino
@@ -0,0 +1,44 @@
+/*
+ * Steve's Template
+ * General Commentst and Logic Narrative
+ *
+ * Pin Assigmnent Chart
+ * 
+ * Variable List
+ * 
+ * 
+ */
+
+
+// Explicitly include Arduino.h to insure compatability if using other than the Arduino IDE
+#include "Arduino.h"
+
+// ************************************************
+//    Include other libraries and header files as needed.
+
+// ************************************************
+/*  Setup for DIP switch pins for devices
+ *  const int devicePin1 = 2;   // Device 1 is connected to pin 2
+ *   
+ *  Define Variables Needed
+ *
+ *  int variableState1 = 0;    // Variable to ....
+ * 
+ */
+
+
+// ************************************************
+//                     SETUP()
+// ************************************************
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+// ************************************************
+//                     LOOP()
+// ************************************************
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
